e417c72299286d9e823667bb745a1fb1c644a670
kartman88 <51677278+kartman88@users.noreply.github.com>
2022-04-07 11:02:23 +0200

    Percorso file
    Ripulita del codice e aggiunta del percorso file locale in modo da non dover sistemarlo ogni volta che si cambia dispositivo.

--- visione/visione.py
+++ visione/visione.py
@@ -1,12 +1,15 @@
+import os
+angolo_x=6
+angolo_y= 11
 
 def cv(pose,landmarks):
-    angolo_x=6
-    angolo_y= 11
+    
     for x in range(len(landmarks)):
         if landmarks[x][0] in range(pose[0]-angolo_x,pose[0]+angolo_x) and landmarks[x][1] in  range(pose[1]+1,pose[1]+angolo_y):
             print(landmarks[x])
 def carica_file(lista):
-    file_coni = open("C:\\Users\\Tano\\Desktop\\visione\\posizione.txt")
+    location_coni = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    file_coni = open(os.path.join(location_coni, 'posizione.txt'))
     for line in file_coni:
         line2 = file_coni.readline()
         lista.append((int(line), int(line2)))
