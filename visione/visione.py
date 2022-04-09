48ede43b5fe0ee26ba6f6e7fe51d633866512f1b
kartman88 <51677278+kartman88@users.noreply.github.com>
2022-04-09 13:16:29 +0200

    triangolo shapely
    Realizzazione dell'area di visione non più con un rettangolo ma con un triangolo che può essere adesso ruotato in base all'orientamento della macchina.

--- visione/visione.py
+++ visione/visione.py
@@ -2,27 +2,25 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 from shapely import affinity
 import os
-angolo_x=10
-angolo_y= 10
-posizione_macchina=(10,10)
-vbs = Point (posizione_macchina[0]-angolo_x, posizione_macchina[1])
-vbd = Point (posizione_macchina[0]+angolo_x, posizione_macchina[1])
-vas = Point (posizione_macchina[0]-angolo_x, posizione_macchina[1]+angolo_y)
-vad = Point (posizione_macchina[0]+angolo_x, posizione_macchina[1]+angolo_y)
-rettangolo = Polygon([[p.x, p.y] for p in [vbs,vas,vad,vbd]]) 
-ruotato= affinity.rotate(rettangolo, 180, 'center')
-print(rettangolo.boundary)
+ANGOLO_X=10
+ANGOLO_Y= 10
+posizione_macchina= Point(10,10)
+orientamento_macchina = 0
+vas = Point (posizione_macchina.x-ANGOLO_X, posizione_macchina.y+ANGOLO_Y)
+vad = Point (posizione_macchina.x+ANGOLO_X, posizione_macchina.y+ANGOLO_Y)
+triangolo = Polygon([[p.x, p.y] for p in [posizione_macchina, vas, vad]]) #i punti del poligono vano messi in ordine in cui va disegnato
+ruotato= affinity.rotate(triangolo, orientamento_macchina, posizione_macchina)  #l'angolo di rotazione ruota in senso antiorario
+print(triangolo.boundary)
 print(ruotato.boundary)
 
-def cv(landmarks):
+def cv(landmarks): #funzione che controlla se un cono si trova all'interno del triangolo di visione 
     
     for x in range(len(landmarks)):
-        #print(landmarks[x])
-        if rettangolo.contains(landmarks[x]) :
-            print(landmarks[x])
+        if triangolo.contains(landmarks[x]) :
+            print("Vedo i coni in posizione:\n", landmarks[x], "\n")
 
 
-def carica_file(lista):
+def carica_file(lista): #funzione per caricare dal file la lista dei coni
     location_coni = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     file_coni = open(os.path.join(location_coni, 'posizione.txt'))
     for line in file_coni:
